840e9c0388aef9c838f27142e6d87f4e9aebbc0f
litblc <huaixiu.zhen@gmail.com>
2018-09-26 10:57:48 +0000

    Apply fixes from StyleCI

--- app/Models/UsersPostsLike.php
+++ app/Models/UsersPostsLike.php
@@ -10,6 +10,6 @@ class UsersPostsLike extends Model
     protected $table = 'users_posts_like';
 
     protected $fillable = [
-        'user_id', 'post_id', 'action'
+        'user_id', 'post_id', 'action',
     ];
 }
