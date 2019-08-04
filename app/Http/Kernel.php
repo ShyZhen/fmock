6aba218c3456674788f090d84f556713b1fc83ec
huaixiu.zhen <huaixiu.zhen@gmail.com>
2019-08-04 21:32:23 +0800

    # cors update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,8 @@ class Kernel extends HttpKernel
         // 将空字符串变成null
         //\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        // 需要全局注册跨域中间件 MDBUG
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -41,9 +43,9 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            // 跨域路由中间件
-            \Barryvdh\Cors\HandleCors::class,
-//            \App\Http\Middleware\Cors::class,
+            // 跨域路由中间件 需要全局注册才起作用
+            // \Barryvdh\Cors\HandleCors::class,
+            // \App\Http\Middleware\Cors::class,
         ],
     ];
 
