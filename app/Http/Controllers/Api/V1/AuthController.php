b337c47fc0f738a6b361fe100deda90f2a633db2
litblc <ShyZhen@users.noreply.github.com>
2021-04-16 09:10:58 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -379,6 +379,7 @@ class AuthController extends Controller
      * 快捷登录，发送普通短信验证码
      *
      * @param Request $request
+     *
      * @return \Illuminate\Http\JsonResponse
      */
     public function quickLoginCode(Request $request): \Illuminate\Http\JsonResponse
@@ -389,7 +390,6 @@ class AuthController extends Controller
         $type = $this->authService->regexAccountType($account);
 
         if ($type) {
-
             return $this->authService->sendLoginCode($account, $type);
         } else {
             return response()->json(
